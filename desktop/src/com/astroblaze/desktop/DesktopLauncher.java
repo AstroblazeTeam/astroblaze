2c96b8eba8cd7d8bbccc8c38802e785afba75665
Alexey Kim <git@karmaflux.com>
2021-05-29 10:17:31 +0300

    Libgdx tool generated project base

---
+++ desktop/src/com/astroblaze/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.astroblaze.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.astroblaze.AstroblazeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AstroblazeGame(), config);
+	}
+}
